e0ab6ef6fd5aa7bff6a8de5749096d0b2ede5691
Gabriel Paolini <paolini396@gmail.com>
2020-12-08 19:03:01 -0300

    feat: relationships models

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -4,6 +4,13 @@
 const Model = use('Model')
 
 class Project extends Model {
+  user () {
+    return this.belongsTo('App/Models/User') // pertence a um usuário
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task') // um projeto pode ter várias tarefas associadas a ele.
+  }
 }
 
 module.exports = Project
--- app/Models/Task.js
+++ app/Models/Task.js
@@ -4,6 +4,17 @@
 const Model = use('Model')
 
 class Task extends Model {
+  project () {
+    return this.belongsTo('App/Models/Project') // a tarefa pertence a um projeto
+  }
+
+  user () {
+    return this.belongsTo('App/Models/User')
+  }
+
+  file () {
+    return this.belongsTo('App/Models/File')
+  }
 }
 
 module.exports = Task
