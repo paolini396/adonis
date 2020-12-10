89975fb5f702819b75adf4f62c57ec43ed6f5e37
Gabriel Paolini <paolini396@gmail.com>
2020-12-10 09:17:54 -0300

    feat: controller Task

--- start/routes.js
+++ start/routes.js
@@ -9,8 +9,10 @@ Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
 
 Route.get('/files/:id', 'FileController.show')
+
 Route.group(() => {
   Route.post('/files', 'FileController.store')
 
   Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly() // caso o registro precise de um registro PAI
 }).middleware(['auth'])
